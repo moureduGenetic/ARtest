b4cbab033517ff23e212d2491774d77a6d1d7ba3
Fausto Davila <diukescd@gmail.com>
2019-01-31 17:30:03 +0100

    Añadida animación al detectar target

---
+++ Assets/_Assets/Scripts/TargetDetection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Vuforia;
+using UnityEngine;
+
+public class TargetDetection : MonoBehaviour, ITrackableEventHandler
+{
+    [SerializeField]
+    private Animator bustAnimator;
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }        
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Executes this code when target is tracked
+            bustAnimator.enabled = true;
+        }
+        else
+        {
+            // Executes this code when target is no longer tracked
+            
+        }
+    }
+}
